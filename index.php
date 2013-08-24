e3f38dbd6b6f3b983731591615bc0332faf90089
ber34 <ber34@o2.pl>
2013-08-25 00:13:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+/*
+* Css_responsive & CssBrowser & CssVersion & Protocol Open Graph Facebook &  framework Bootstrap 
+*
+* PHP 5.1
+* @version 1.0
+* @Added support for the Protocol Open Graph Facebook
+* @Added support for the framework Bootstrap 
+* @license Apache License, Version 2.0.
+* @author Adam Berger <ber34@o2.pl>
+* @Site www.joomla-cms.com.pl
+*/
+
 require_once (dirname(__FILE__) . '/lib/'.basename('class_cssresponsiwe.php'));
 $browser = new CssResponsive();
 /*
